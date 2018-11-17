4feef42bda733f739aefdcc857d1644a72f6df00
ojathelonius <louis.johanet@epfedu.fr>
2018-11-17 16:28:46 +0100

    Add basic structural components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  FooterComponent,
+  HeaderComponent
+} from './shared';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
