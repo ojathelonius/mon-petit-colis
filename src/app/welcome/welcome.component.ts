eec053d02b05baafed28966f10efc9ffa0c70e9e
ojathelonius <louis.johanet@epfedu.fr>
2018-11-17 18:56:16 +0100

    Add fake loader and text curvature

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import CircleType from 'circletype';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+
+
+    /* Text curvature */
+    const circleTitle = new CircleType(document.getElementById('curved-title'));
+    circleTitle.radius(400).dir(1);
+
+    const circleLoader = new CircleType(document.getElementById('curved-loader'));
+    circleLoader.radius(400).dir(-1);
+
+    /* Fake loader */
+    setTimeout(() => {
+      this.router.navigate(['/home'])
+    }, 3000);
   }
 
 }
