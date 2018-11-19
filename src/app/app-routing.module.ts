b9cb801514e34664bd65485d578e5752bcade6fb
ojathelonius <louis.johanet@epfedu.fr>
2018-11-20 00:17:38 +0100

    Add create, orders, support and cart modules & components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,22 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: './home/home.module#HomeModule'
+  },
+  {
+    path: 'create',
+    loadChildren: './create/create.module#CreateModule'
+  },
+  {
+    path: 'orders',
+    loadChildren: './orders/orders.module#OrdersModule'
+  },
+  {
+    path: 'support',
+    loadChildren: './support/support.module#SupportModule'
+  },
+  {
+    path: 'cart',
+    loadChildren: './cart/cart.module#CartModule'
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
