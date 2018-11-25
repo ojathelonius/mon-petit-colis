45bdb1a69c54619952c65859c86c98893e0d9945
ojathelonius <louis.johanet@epfedu.fr>
2018-11-25 20:07:57 +0100

    Set create page structure with angular material

--- src/app/create/create.module.ts
+++ src/app/create/create.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { CreateRoutingModule } from './create-routing.module';
 import { CreateComponent } from './create.component';
 import { EditorModule } from '../editor/editor.module';
+import { SharedModule } from '../shared/shared.module';
+import { MatCardModule } from '@angular/material';
 
 @NgModule({
   declarations: [CreateComponent],
   imports: [
     CommonModule,
     CreateRoutingModule,
-    EditorModule
+    EditorModule,
+    SharedModule,
+    MatCardModule
   ]
 })
 export class CreateModule { }
