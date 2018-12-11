9bd7f6dde97ace9947365884dde87b68a081ec8a
ojathelonius <louis.johanet@epfedu.fr>
2018-12-11 23:36:00 +0100

    Add orders view with example package

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrdersComponent } from './orders.component';
 import { SharedModule } from '../shared/shared.module';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [OrdersComponent],
   imports: [
     CommonModule,
     OrdersRoutingModule,
-    SharedModule
+    SharedModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule
   ]
 })
 export class OrdersModule { }
