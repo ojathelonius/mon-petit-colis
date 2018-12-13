88eda384c495f41f32aaa8da0b1a584a80fa559b
ojathelonius <louis.johanet@epfedu.fr>
2018-12-13 23:15:08 +0100

    Add dummy design service request view and AI module in editor view

--- src/app/service/service.module.ts
+++ src/app/service/service.module.ts
@@ -2,12 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { ServiceComponent } from 'src/app/service/service.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MccColorPickerModule } from 'material-community-components';
 
 @NgModule({
   declarations: [ServiceComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatRadioModule,
+    MccColorPickerModule
   ]
 })
 export class ServiceModule { } 
